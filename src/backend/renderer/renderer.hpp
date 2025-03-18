ef8669e8fafdfbabb513927017add88207b65dec
tift0 <67962635+tift0@users.noreply.github.com>
2025-03-19 00:53:18 +0500

    update renderer.hpp

--- src/backend/renderer/renderer.hpp
+++ src/backend/renderer/renderer.hpp
@@ -16,16 +16,16 @@ namespace backend {
 		void send_buffer( U8G2& buffer ) { buffer.sendBuffer( ); }
 
 		void draw_string( U8G2& buffer, math::vec2_t pos, const String& str ) {
-            buffer.setFont( u8g2_font_ncenB08_tr );
-            buffer.drawStr( pos.x( ), pos.y( ), str.c_str( ) );
+            		buffer.setFont( u8g2_font_ncenB08_tr );
+            		buffer.drawStr( pos.x( ), pos.y( ), str.c_str( ) );
 		}
 
 		void draw_line( U8G2& buffer, math::vec2_t start, math::vec2_t end ) {
-            buffer.drawLine( start.x( ), start.y( ), end.x( ), end.y( ) );
+            		buffer.drawLine( start.x( ), start.y( ), end.x( ), end.y( ) );
 		}
 
 		void draw_rect( U8G2& buffer, math::vec2_t pos, math::vec2_t size ) {
-            buffer.drawBox( pos.x( ), pos.y( ), size.x( ), size.y( ) );
+            		buffer.drawBox( pos.x( ), pos.y( ), size.x( ), size.y( ) );
 		}
 	};
 
